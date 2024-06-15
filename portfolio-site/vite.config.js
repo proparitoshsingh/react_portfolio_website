32963d70b053504df6588d39757c690a422e36a5
Paritosh Singh <paritoshsingh1609@gmail.com>
2024-06-15 12:15:59 +0530

    fix:typo

--- portfolio-site/vite.config.js
+++ portfolio-site/vite.config.js
@@ -1,10 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     watch: {
       usePolling: true, // Enable polling for file changes
     },
   },
-});
+  base: '/react_portfolio_website/',
+  plugins: [react()]
+})
